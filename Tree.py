badefc2362ce3d274cbac289825ee92b46ce2ab6
frankwalkar <84692243+frankwalkar@users.noreply.github.com>
2021-05-24 15:55:42 +1000

    Update Tree.py

--- Tree.py
+++ Tree.py
@@ -4,18 +4,11 @@ import mcpi.block as block
 
 mc = minecraft.Minecraft.create()
 x, y, z = mc.player.getPos()
-Floor = [x-2, y-1, z-2, x+2, y-1, z+2, 5]
-roof = [x-2,y+3,z-2,x+2,y+3,z+2, 17]
-def house():
-    mc.setBlocks(Floor)
-    mc.setBlocks(x-2,y,z-2,x-2,y+2,z+2,5) #wall1
-    mc.setBlocks(x-2,y,z-2,x+2,y+2,z-2,5) #wall2
-    mc.setBlocks(x+2,y,z-2,x+2,y+2,z+2,5) #wall3
-    mc.setBlocks(x-2,y,z+2,x+2,y+2,z+2,5) #wall4
-    mc.setBlocks(roof)
-    mc.setBlock(x-2,y+1,z,20) #window1
-    mc.setBlock(x,y+1,z-2,20) #window2
-    mc.setBlock(x+2,y+1,z,20) #window3
-    mc.setBlock(x,y+1,z+2, 0) #door1
-    mc.setBlock(x,y,z+2, 0) #door2
-house()
+
+mc.setBlocks(x-9,y,z,x-9,y+8,z,17)
+mc.setBlocks(x-6,y+1,z+3,x-12,y+2,z-3,18)
+mc.setBlocks(x-7,y+3,z+2,x-11,y+4,z-2,18)
+mc.setBlocks(x-8,y+5,z+1,x-10,y+7,z-1,18)
+mc.setBlocks(x-9,y+8,z+1,x-9,y+8,z-1,18)
+mc.setBlocks(x-8,y+8,z,x-10,y+8,z,18)
+mc.setBlock(x-9,y+9,z,18)
